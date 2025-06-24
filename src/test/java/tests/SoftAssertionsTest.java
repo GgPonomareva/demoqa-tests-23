98993be8e68bba60c1df857e507f77f1969aa11a
Galina Ponomareva <li.c@mail.ru>
2025-06-24 20:38:27 +0300

    добавила тест SoftAssertions (ДЗ№3)-убрала скролл

--- src/test/java/tests/SoftAssertionsTest.java
+++ src/test/java/tests/SoftAssertionsTest.java
@@ -31,7 +31,6 @@ public class SoftAssertionsTest {
         $(".markdown-body").$(byText("Soft assertions")).click();
 
         //проверьте что внутри есть пример кода для JUnit5
-        $("#user-content-3-using-junit5-extend-test-class").scrollTo();
         $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
 
         String textJunit5 = """
