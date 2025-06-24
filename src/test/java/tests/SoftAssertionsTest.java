a357fdfbaae77249d87e517b8d27b4c1fbeff3b9
Galina Ponomareva <li.c@mail.ru>
2025-06-24 14:18:08 +0300

    добавила тест SoftAssertions (ДЗ№3)

---
+++ src/test/java/tests/SoftAssertionsTest.java
@@ -0,0 +1,53 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsTest {
+
+    @BeforeAll
+    static void onSettings() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void  selenideTest(){
+        //Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+
+        //Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $(".markdown-body").shouldHave(text("Soft assertions")).click();
+
+        //Откройте страницу SoftAssertions
+        $(".markdown-body").$(byText("Soft assertions")).click();
+
+        //проверьте что внутри есть пример кода для JUnit5
+        $("#user-content-3-using-junit5-extend-test-class").scrollTo();
+        $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+
+        String textJunit5 = """
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """;
+
+        $("#wiki-body").shouldBe(text(textJunit5));
+    }
+}
